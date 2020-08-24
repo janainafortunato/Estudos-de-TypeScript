e0804c0b1eb48fe164ccbef45d97282359fda3ee
JanainaFortunato <janaina.projetos@live.com>
2020-08-23 22:14:15 -0300

    estudos dos arquivos tsconfig.json

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Estudos-de-TypeScript
 Desenvolvendo novos projetos com TypeScript
+
+npm start
+tsc -w
---
+++ curso-typescript/compilador/compilador.ts
@@ -0,0 +1,28 @@
+let canal: string = 'Gaveta'
+let inscritos: number = 610234
+
+//canal = inscritos
+console.log(`Canal = ${canal}`)
+
+//let nome = 'Pedro'
+
+
+function soma(a: any, b: any){
+    return a + b
+}
+
+let qualquerCoisa
+qualquerCoisa = 12
+qualquerCoisa = 'abc'
+
+function saudar(isManha: boolean, horas: number): string{
+    let saudacao: string
+    if(isManha) {
+        saudacao = 'Bom Dia!'
+
+    }else{
+        saudacao = 'Tenha uma boa vida!'
+    }
+    return saudacao
+}
+
